cb3e8d94629e21d5aed9ed4a8b5bdbaf1bbb4db5
Chlorokiller <115887900+Chlorokiller@users.noreply.github.com>
2022-10-19 23:35:28 +0300

    Лабораторная работа 3

---
+++ task_8.py
@@ -0,0 +1,11 @@
+money_capital = 10000
+salary = 5000
+spend = 6000
+increase = 0.05
+
+month = 0 # количество месяцев, которое представляется возможным прожить
+while money_capital > spend:
+    money_capital = money_capital + salary - spend
+    spend *= (1 + increase) ** month
+    month += 1
+print(month)
